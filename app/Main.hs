0a599158d0a516c5c15582816ab1d76bde335d66
Tyler Sommer <sommertm@gmail.com>
2017-04-13 17:42:30 -0600

    Functioning arg and editor parsing.

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE NamedFieldPuns #-}
 module Main where
 
 import Data.Aeson
 import Data.String
+import Data.List
 import System.Directory
 import System.Environment
 import System.Exit
@@ -12,23 +12,37 @@ import System.Process
 
 import Pond.Data
 
-type State = (Either String (Ripple -> String -> Ripple), Ripple)
+import qualified Data.ByteString.Lazy.Char8 as LS
 
-parseTagArg :: Ripple -> String -> Ripple
-parseTagArg r arg = do
-  let facet = fromString arg :: Facet
-  r { facets = (facets r) ++ [facet] }
+data State = ParseAny | ParseTag | ParseDesc
 
-parseShortArg :: Ripple -> String -> Ripple
-parseShortArg r arg = r { summary = arg }
+parseArg :: State -> Ripple -> [String] -> Ripple
+parseArg ParseAny r ("-t":xs) = parseArg ParseTag r xs
+parseArg ParseAny r (s:xs)    = parseArg ParseAny (r { summary = s }) xs
+parseArg ParseTag r (s:xs)    = parseArg ParseAny (r { facets = (facets r) ++ [(fromString s :: Facet)] }) xs
+parseArg _ r _                = r
 
-configureCommand :: State -> String -> State
-configureCommand (Left desc, c) arg = do
-  case arg of
-    "-t" -> (Right parseTagArg, c)
-    otherwise -> (Left "short", parseShortArg c arg)
-configureCommand (Right fn, c) arg =
-  (Left "reset", fn c arg)
+parseArgs :: [String] -> Ripple
+parseArgs argv = parseArg ParseAny (Ripple "" Nothing []) argv
+
+parseEditorLine :: State -> Ripple -> [String] -> Ripple
+parseEditorLine ParseAny r (s:xs)        = parseEditorLine ParseDesc (r { summary = s }) xs
+parseEditorLine ParseDesc r ("":xs)      = parseEditorLine ParseDesc r xs
+parseEditorLine ParseDesc r (("#"):xs)   = parseEditorLine ParseDesc r xs
+parseEditorLine ParseDesc r (('#':_):xs) = parseEditorLine ParseDesc r xs
+parseEditorLine ParseDesc r (('F':'a':'c':'e':'t':':':' ':s):xs) =
+  parseEditorLine ParseTag r ([s] ++ xs)
+parseEditorLine ParseDesc r (s:xs) =
+  case (description r) of
+    Just desc -> parseEditorLine ParseDesc (r { description = Just (desc ++ "\n" ++ s) }) xs
+    _         -> parseEditorLine ParseDesc (r { description = (Just s) }) xs
+
+parseEditorLine ParseTag r (s:xs) =
+  parseEditorLine ParseDesc (r { facets = (facets r) ++ [(fromString s :: Facet)] }) xs
+parseEditorLine _ r _ = r
+
+parseEditorLines :: [String] -> Ripple
+parseEditorLines lines = parseEditorLine ParseAny (Ripple "" Nothing []) lines
 
 usageText :: String
 usageText = unlines
@@ -68,60 +82,28 @@ openEditor ripple = do
   hClose tmp
   (_, Just hout, _, _) <-
     createProcess (proc "bash" ["-c", ("$EDITOR " ++ file ++ " && cat " ++ file)]){ std_out = CreatePipe }
-  hGetContents hout >>= (parseEditor ripple)
-
-parseEditor :: Ripple -> String -> IO Ripple
-parseEditor ripple input = return $ snd (foldl (parseEditorRipple) (Right parseEditorSummary, ripple) (lines input))
-
-type EditorParser = Ripple -> String -> Maybe Ripple
-type EditorState = (Either String EditorParser, Ripple)
-
-parseEditorSummary :: Ripple -> String -> Maybe Ripple
-parseEditorSummary ripple line =
-  case line of
-    '#':_ -> Nothing
-    s -> Just (ripple { summary = s })
-
-parseEditorNoOp :: Ripple -> String -> Maybe Ripple
-parseEditorNoOp ripple _ = Nothing
-
-nextEditorParser :: EditorParser -> EditorParser
-nextEditorParser curr = parseEditorNoOp
-
-parseEditorRipple :: EditorState -> String -> EditorState
-parseEditorRipple state line = do
-  case state of
-    (Left desc, ripple) -> (Left desc, ripple)
-    (Right fn, ripple) -> do
-      case fn ripple line of
-        Just r -> (Right (nextEditorParser fn), r)
-        otherwise -> (Right fn, ripple)
+  contents <- hGetContents hout
+  return $ parseEditorLines (lines contents)
 
 printUsageAndExit :: IO Ripple
 printUsageAndExit = do
   putStr usageText
   exitWith (ExitFailure 1)
 
-parseArgs :: [String] -> IO Ripple
-parseArgs [] = printUsageAndExit
-parseArgs argv =
-  case head argv of
-    "add" -> do
-      let s = foldl configureCommand (Left "start", Ripple "" Nothing []) (tail argv)
-      case s of
-        (Left desc, ripple) -> do
-          if summary ripple == ""
-            then openEditor ripple
-            else
-              return $ ripple
-        (Right _, _) -> do
-          putStrLn "expected value, got nothing"
-          printUsageAndExit
-    unk -> do
-      putStrLn ("unknown command: " ++ unk)
-      printUsageAndExit
-
 main :: IO ()
 main = do
-  cmd <- getArgs >>= parseArgs
-  print cmd
+  r <- getArgs >>= (\argv ->
+    case argv of
+      "add":xs -> do
+        let s = parseArgs xs
+        case s of
+          (Ripple "" _ _) -> do
+            openEditor s
+          _ -> do
+            return $ s
+      unk:s -> do
+        putStrLn ("unknown command: " ++ unk)
+        printUsageAndExit
+      _ -> printUsageAndExit)
+  print r
+  LS.putStrLn (encode r)
